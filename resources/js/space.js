188c800b637f8760701e8922e128d59b39ca04ba
jesutobi <jesutobit@gmail.com>
2022-01-18 14:15:41 +0100

    correcting a css error

--- resources/js/space.js
+++ resources/js/space.js
@@ -3,7 +3,7 @@
     
       {
         name: "MOON",
-        images:  "destination/image-moon.png",
+        images:  "../assests/image-moon.png",
         description: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
         distance: "384,400 km",
         travel: "3 days",
@@ -11,7 +11,7 @@
       },
       {
         name: "MARS",
-        images: "destination/image-mars.png",
+        images: "../assests/image-mars.png",
         description: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
         distance: "225 mil. km",
         travel: "9 months",
@@ -19,7 +19,7 @@
       },
       {
         name: "EUROPA",
-        images:"destination/image-europa.png",
+        images:"../assests/image-europa.png",
         description: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
         distance: "628 mil. km",
         travel: "3 years",
@@ -27,7 +27,7 @@
       },
       {
         name: "TITAN",
-        images:"destination/image-titan.png",
+        images:"../assests/image-titan.png",
         description: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
         distance: "1.6 bil. km",
         travel: "7 years",
